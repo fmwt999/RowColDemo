8d21bdf208b1e9609b18e9c1b28acfb1e4e49a25
fmwt999 <138676550+fmwt999@users.noreply.github.com>
2026-09-22 11:58:25 +0700

    alignBy

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.layout.LastBaseline
 
 
 class MainActivity : ComponentActivity() {
@@ -38,8 +39,8 @@ class MainActivity : ComponentActivity() {
 fun MainScreen(modifier: Modifier = Modifier) {
     Row {
         Text(
-            text = "Large Text",
-            Modifier.alignByBaseline(),
+            text = "Large Text\n\nMore Text",
+            Modifier.alignBy(LastBaseline),
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold
         )
@@ -51,6 +52,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
         )
     }
 }
+
 @Composable
 fun TextCell(text: String, modifier: Modifier = Modifier) {
     val cellModifier = modifier
